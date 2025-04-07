cbe8a296153531aca7c9b59185eef27489488e2f
JLDJadr <jose.luis@adrbalears.es>
2025-04-07 12:36:29 +0200

    Proyecto Base creado con dependencias básicas importadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroComponent } from './registro/registro.component';
+import { SeguimientoComponent } from './seguimiento/listado/seguimiento.component';
+import { BodyComponent } from './body/body.component';
+import { SeguimientoDetallesComponent } from './seguimiento/detalles/detalles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BodyComponent },
+  { path: 'registro', component: RegistroComponent },
+  { path: 'seguimiento', component: SeguimientoComponent },
+  { path: 'seguimiento/:id', component: SeguimientoDetallesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BodyComponent } from './body/body.component';
+import { RegistroComponent } from './registro/registro.component';
+import { SeguimientoDetallesComponent } from './seguimiento/detalles/detalles.component';
+import { SeguimientoComponent } from './seguimiento/listado/seguimiento.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent,
+    SeguimientoComponent,
+    BodyComponent,
+    SeguimientoDetallesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    AppRoutingModule,
+    MatFormFieldModule,
+    MatTableModule,
+    MatRadioModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
